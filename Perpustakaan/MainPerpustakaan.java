2b5ec044e13adb942ea074dd9b6315bfdd655d37
Nya <nindyaratnaa@gmail.com>
2025-03-13 09:16:39 +0700

    Update MainPerpustakaan.java

--- Perpustakaan/MainPerpustakaan.java
+++ Perpustakaan/MainPerpustakaan.java
@@ -1,3 +1,11 @@
+/* Suatu perpustakaan di kampus X memiliki banyak koleksi buku, dan buku buku tersebut dikategorikan
+berdasarkan jenisnya. Ada 7 kategori dalam perpustakaan tersebut, yaitu teknologi, filsafat, sejarah,
+agama, psikologi, politik dan fiksi. Setiap kategori pastilah memiliki banyak buku. Setiap buku ditulis oleh
+setidaknya 1 penulis, walaupun tak menutup kemungkinan buku tersebut ditulis oleh banyak penulis.
+Dari studi kasus diatas, tentukan entitas-entitas yang terlibat beserta propertiesnya dan implementasikan
+kedalam kode program, serta tampilkan ke layar nilai dari properties dari entitas tersebut. (Minimal tiap
+kategori ada 5 buku) */
+
 package Perpustakaan;
 
 public class MainPerpustakaan {
